c36e703634984901d63958ffd1c5fcce1d17accc
Ingrid Rebecca Abraham <ingridra@met.no>
2022-06-20 13:22:07 +0200

    put spacebar on both sides of ferris layout

--- keymap.c
+++ keymap.c
@@ -112,12 +112,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    _______, _______, KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN,
     HOME_A,  HOME_R,  HOME_S,  HOME_T,  KC_D,    _______, _______, KC_H,    HOME_N,  HOME_E,  HOME_I,  HOME_O,
     KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______, _______, KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH,
-    _______, _______, _______, L3,      KC_SPC,  _______, _______, KC_BSPC, L2,      _______, _______, _______
+    _______, _______, _______, L3,      KC_SPC,  _______, _______, KC_SPC,  L2,      _______, _______, _______
 ),
 
 [_L2] = LAYOUT_planck_grid(
-    KC_0,    KC_1,    KC_2,    KC_3,    KC_GRV,  _______, _______, KC_LALT, KC_LCBR, KC_RCBR, KC_LBRC, KC_RBRC,
-    KC_MINS, KC_4,    KC_5,    KC_6,    KC_QUOT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_ESC,
+    KC_0,    KC_1,    KC_2,    KC_3,    KC_GRV,  _______, _______, KC_LALT, KC_LBRC, KC_RBRC, KC_TAB,  KC_ESC,
+    KC_MINS, KC_4,    KC_5,    KC_6,    KC_QUOT, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RGHT, KC_BSPC,
     KC_EQL,  KC_7,    KC_8,    KC_9,    KC_BSLS, _______, _______, KC_LGUI, KC_LPRN, KC_RPRN, KC_TAB,  KC_ENT,
     _______, _______, _______, _______, KC_LSFT, _______, _______, KC_LCTL, L3,      _______, _______, _______
 ),
