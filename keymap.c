dc46dc9202af9b3a177f96a5db1b6b5cfe4a745a
Ingrid <git@ingrids.email>
2023-02-07 18:04:57 +0100

    add EE_CLR on adjust

--- keymap.c
+++ keymap.c
@@ -17,7 +17,6 @@
 #include QMK_KEYBOARD_H
 #include "muse.h"
 
-
 enum planck_layers {
   _COLEMAK,
   _QWERTY,
@@ -87,7 +86,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     QK_BOOT, KC_F1,   KC_F2,   KC_F3,   KC_F4,   MU_MOD,  RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD,
     COLEMAK, KC_F5,   KC_F6,   KC_F7,   KC_F8,   MUV_DE,  RGB_TOG, RGB_MOD, AU_ON,   AU_OFF,  AG_NORM, AG_SWAP,
     QWERTY,  KC_F9,   KC_F10,  KC_F11,  KC_F12,  MUV_IN,  MU_ON,   MU_OFF,  MI_ON,   MI_OFF,  TERM_ON, TERM_OFF,
-    DEBUG,   _______, _______, _______, _______, BACKLIT, BACKLIT, _______, _______,  _______, _______, _______
+    DEBUG,   EE_CLR,  _______, _______, _______, BACKLIT, BACKLIT, _______, _______,  _______, _______, _______
 )
 };
 
