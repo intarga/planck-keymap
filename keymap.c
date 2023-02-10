ace13d7b60bd268e6b7eceb65f529f80343725e0
Ingrid <git@ingrids.email>
2023-02-10 16:13:11 +0100

    switch shift and raise back

--- keymap.c
+++ keymap.c
@@ -62,13 +62,13 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_LCTL, KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_QUOT, KC_LALT,
     KC_ESC,  KC_A,    HOME_R,  HOME_S,  HOME_T,  KC_D,    KC_H,    HOME_N,  HOME_E,  HOME_I,  KC_O,    KC_BSPC,
     KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    _______, _______, _______, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LSFT, _______, _______, _______
+    _______, _______, _______, KC_LGUI, KC_LSFT, KC_SPC,  KC_SPC,  LOWER,   RAISE,   _______, _______, _______
 ),
 [_QWERTY] = LAYOUT_planck_grid(
     KC_LCTL, KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_QUOT,
     KC_ESC,  KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_BSPC,
     KC_TAB,  KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT ,
-    _______, _______, _______, KC_LGUI, RAISE,   KC_SPC,  KC_SPC,  LOWER,   KC_LSFT, _______, _______, _______
+    _______, _______, _______, KC_LGUI, KC_LSFT, KC_SPC,  KC_SPC,  LOWER,   RAISE,   _______, _______, _______
 ),
 [_LOWER] = LAYOUT_planck_grid(
     _______, _______, _______, KC_SCLN, KC_GRV,  NO_AA,   _______, CTRL_U,  CTRL_D,  _______, KC_TAB,  _______,
